1674c7194ffe18e22ad472921261a5e537091fcd
Lucie-Lanssade <lucie.lanssade@gmail.com>
2023-04-13 18:56:53 +0200

    first commit

---
+++ src/components/Title.js
@@ -0,0 +1,10 @@
+function Title({ title, subTitle }) {
+  return (
+    <div class="section-title">
+      <h2>
+        {title} <span>{subTitle}</span>
+      </h2>
+    </div>
+  );
+}
+export default Title;
